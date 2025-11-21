6d131512ff5135397e64b92ff734bf49052f445f
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-11-21 11:45:09 +0500

    make files and basic setup

---
+++ src/components/UserContainer.tsx
@@ -0,0 +1,14 @@
+import UserForm from "./UserForm"
+import UserList from "./UserList"
+
+
+const UserContainer = () => {
+  return (
+    <div>
+        <UserList />
+        <UserForm />
+    </div>
+  )
+}
+
+export default UserContainer
